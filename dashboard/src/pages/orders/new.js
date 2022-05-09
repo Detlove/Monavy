81ea56abb1b9842aa4d87f277e930baafcdac497
David Eusebio <davidetlove@gmail.com>
2022-05-09 18:34:50 -0500

    unusual commir

--- dashboard/src/pages/orders/new.js
+++ dashboard/src/pages/orders/new.js
@@ -3,14 +3,14 @@ import { Container } from '@mui/material'
 // routes
 import { PATH_DASHBOARD } from '@routes/paths'
 // hooks
-import useSettings from '../../../hooks/useSettings'
+import useSettings from '@hooks/useSettings'
 // layouts
-import Layout from '../../../layouts'
+import Layout from '@layouts/index'
 // components
-import Page from '../../../components/Page'
-import HeaderBreadcrumbs from '../../../components/HeaderBreadcrumbs'
+import Page from '@components/Page'
+import HeaderBreadcrumbs from '@components/HeaderBreadcrumbs'
 // sections
-import InvoiceNewEditForm from '../../../sections/@dashboard/invoice/new-edit-form'
+import InvoiceNewEditForm from '@sections/@dashboard/invoice/new-edit-form'
 
 // ----------------------------------------------------------------------
 
